585e5ac29e87825f8a1f293f9657dd6e46293c88
Michael Woolnough <michael.woolnough@gmail.com>
2018-06-20 13:38:00 +0100

    updated to new buffer code

--- sslproxy/ssl.go
+++ sslproxy/ssl.go
@@ -5,7 +5,7 @@ import (
 
 	"vimagination.zapto.org/byteio"
 	"vimagination.zapto.org/errors"
-	"vimagination.zapto.org/reverseproxy/internal/buffer"
+	"vimagination.zapto.org/memio"
 )
 
 const Name = "SSL"
@@ -14,96 +14,100 @@ var Service service
 
 type service struct{}
 
-func (service) GetServerName(c io.Reader, buf *buffer.Buffer) (string, error) {
-	_, err := io.ReadFull(c, buf.LimitedBuffer[:5])
+func (service) GetServerName(c io.Reader, buf []byte) (int, []byte, error) {
+	mbuf := memio.Buffer(buf[:5])
+	n, err := io.ReadFull(c, mbuf)
 	if err != nil {
-		return "", err
+		return n, nil, err
 	}
 	r := byteio.StickyBigEndianReader{
-		Reader: buf,
+		Reader: &mbuf,
 	}
 	if r.ReadUint8() != 22 {
-		return "", ErrNoHandshake
+		return n, nil, ErrNoHandshake
 	}
 
-	buf.Skip(1) // skip major version
-	buf.Skip(1) // skip minor version
+	mbuf = mbuf[1:] // skip major version
+	mbuf = mbuf[1:] // skip minor version
 
 	length := r.ReadUint16()
 
-	if cap(buf.LimitedBuffer) < 5+int(length) {
-		return "", io.ErrShortBuffer
+	if cap(mbuf) < int(length) {
+		return n, nil, io.ErrShortBuffer
 	}
 
-	_, err = io.ReadFull(buf, buf.LimitedBuffer[5:5+length])
+	mbuf = mbuf[:length]
+	m, err := io.ReadFull(c, mbuf)
 	if err != nil {
-		return "", err
+		return n, nil, err
 	}
+	n += m
 
 	if r.ReadUint8() != 1 {
-		return "", ErrNoClientHello
+		return n, nil, ErrNoClientHello
 	}
 
 	l := r.ReadUint24()
 	if l != uint32(length)-4 {
-		return "", errors.WithContext("error reading body: ", ErrInvalidLength)
+		return n, nil, errors.WithContext("error reading body: ", ErrInvalidLength)
 	}
 
-	buf.Skip(1) // skip major version
-	buf.Skip(1) // skip minor version
+	mbuf = mbuf[1:] // skip major version
+	mbuf = mbuf[1:] // skip minor version
 
-	buf.Skip(4)  // skip gmt_unix_time
-	buf.Skip(28) // skip random_bytes
+	mbuf = mbuf[4:]  // skip gmt_unix_time
+	mbuf = mbuf[28:] // skip random_bytes
 
 	sessionLength := r.ReadUint8()
-	if sessionLength > 32 || len(buf.LimitedBuffer) < int(sessionLength) {
+	if sessionLength > 32 || len(mbuf) < int(sessionLength) {
 		// invalid length
-		return "", errors.WithContext("error reading sesion id: ", ErrInvalidLength)
+		return n, nil, errors.WithContext("error reading sesion id: ", ErrInvalidLength)
 	}
-	buf.Skip(int(sessionLength)) // skip session id
+	mbuf = mbuf[sessionLength:] // skip session id
 
 	cipherSuiteLength := r.ReadUint16()
-	if cipherSuiteLength == 0 || len(buf.LimitedBuffer) < int(cipherSuiteLength) {
+	if cipherSuiteLength == 0 || len(mbuf) < int(cipherSuiteLength) {
 		// invalid length
-		return "", errors.WithContext("error reading cipher suites: ", ErrInvalidLength)
+		return n, nil, errors.WithContext("error reading cipher suites: ", ErrInvalidLength)
 	}
-	buf.Skip(int(cipherSuiteLength)) // skip cipher suites
+	mbuf = mbuf[cipherSuiteLength:] // skip cipher suites
 
 	compressionMethodLength := r.ReadUint8()
-	if compressionMethodLength < 1 || len(buf.LimitedBuffer) < int(compressionMethodLength) {
-		return "", errors.WithContext("error reading compressions: ", ErrInvalidLength)
+	if compressionMethodLength < 1 || len(mbuf) < int(compressionMethodLength) {
+		return n, nil, errors.WithContext("error reading compressions: ", ErrInvalidLength)
 	}
-	buf.Skip(int(compressionMethodLength)) // skip compression methods
+	mbuf = mbuf[compressionMethodLength:] // skip compression methods
 
 	extsLength := r.ReadUint16()
-	if len(buf.LimitedBuffer) < int(extsLength) {
-		return "", errors.WithContext("error reading extensions: ", ErrInvalidLength)
+	if len(mbuf) < int(extsLength) {
+		return n, nil, errors.WithContext("error reading extensions: ", ErrInvalidLength)
 	}
+	mbuf = mbuf[:extsLength]
 
-	for len(buf.LimitedBuffer) > 0 {
+	for len(mbuf) > 0 {
 		extType := r.ReadUint16()
 		extLength := r.ReadUint16()
-		if len(buf.LimitedBuffer) < int(extLength) {
-			return "", errors.WithContext("error reading extension: ", ErrInvalidLength)
+		if len(mbuf) < int(extLength) {
+			return n, nil, errors.WithContext("error reading extension: ", ErrInvalidLength)
 		}
 		if extType == 0 { // server_name
 			l := r.ReadUint16()
 			if l != extLength-2 {
-				return "", errors.WithContext("error reading server name extension: ", ErrInvalidLength)
+				return n, nil, errors.WithContext("error reading server name extension: ", ErrInvalidLength)
 			}
 
-			buf.Skip(1) // skip name_type
+			mbuf = mbuf[1:] // skip name_type
 
 			nameLength := r.ReadUint16()
-			if len(buf.LimitedBuffer) < int(nameLength) {
-				return "", errors.WithContext("error reading server name: ", ErrInvalidLength)
+			if len(mbuf) < int(nameLength) {
+				return n, nil, errors.WithContext("error reading server name: ", ErrInvalidLength)
 			}
-			return string(buf.LimitedBuffer[:nameLength]), nil
+			return n, mbuf[:nameLength], nil
 		} else {
-			buf.Skip(int(extLength))
+			mbuf = mbuf[extLength:]
 		}
 	}
-	return "", ErrNoName
+	return n, nil, ErrNoName
 }
 
 func (service) Service() string {
