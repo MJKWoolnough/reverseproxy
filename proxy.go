650082488bc8a22aa11e84d54266060b2354191d
Michael Woolnough <michael.woolnough@gmail.com>
2018-06-25 17:17:27 +0100

    start of proxy code

---
+++ proxy.go
@@ -0,0 +1,85 @@
+package reverseproxy
+
+import (
+	"io"
+	"net"
+	"sync"
+
+	"vimagination.zapto.org/errors"
+	"vimagination.zapto.org/reverseproxy/internal/buffer"
+)
+
+type service struct {
+	Service
+	servers map[string]Server
+}
+
+type Proxy struct {
+	l net.Listener
+	p Protocol
+
+	mu       sync.RWMutex
+	services map[string]service
+}
+
+func New(l net.Listener, p Protocol) *Proxy {
+	return &Proxy{
+		l:        l,
+		protocol: p,
+		services: make(map[string]service),
+	}
+}
+
+type Protocol interface {
+	GetServerName(io.Reader, []byte) (int, []byte, error)
+	Name() string
+}
+
+func (p *Proxy) Start() error {
+	go p.run()
+	return nil
+}
+
+func (p *Proxy) run() {
+	for {
+		c, err := p.l.Accept()
+		if err != nil {
+			if eerr, ok := err.(net.Error); ok {
+				if eerr.Temporary() {
+					continue
+				}
+			}
+			break // set error?
+		}
+		go p.handle(c)
+	}
+}
+
+func (p *Proxy) handle(c net.Conn) {
+	buf := buffer.Get()
+	n, name, err := p.p.GetServerName(c, buf)
+	if err != nil {
+		buffer.Put(buf)
+		c.Close()
+		return
+	}
+	p.mu.RLock()
+	serv, ok := p.services[string(name)]
+	p.mu.RUnlock()
+	if !ok {
+		buffer.Put(buf)
+		c.Close()
+		return
+	}
+	serv.handle(c, buf[:n])
+	buffer.Put(buf)
+	c.Close()
+}
+
+func (p *Proxy) Stop() error {
+	return p.l.Close()
+}
+
+const (
+	ErrServerRegistered errors.Error = "server already registered"
+)
