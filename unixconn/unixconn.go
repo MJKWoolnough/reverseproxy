4a11ce58bc70425a762d527ea7ce5d437531f9eb
Michael Woolnough <michael.woolnough@gmail.com>
2021-05-25 14:29:36 +0100

    unixconn will fallback to just trying to create the listener using net package, useful for testing or for when you don't know if the program will be communicating with a reverse proxy

--- unixconn/unixconn.go
+++ unixconn/unixconn.go
@@ -1,9 +1,14 @@
 package unixconn
 
 import (
+	"crypto/tls"
+	"errors"
 	"net"
 	"os"
 	"sync"
+
+	"vimagination.zapto.org/byteio"
+	"vimagination.zapto.org/memio"
 )
 
 var (
@@ -19,4 +24,40 @@ func init() {
 			uc = c
 		}
 	}
+}
+
+func ListenHTTP(network, address string) (net.Listener, error) {
+	return requestListener(network, address, false)
+}
+
+func ListenTLS(network, address string, config *tls.Config) (net.Listener, error) {
+	if config == nil || len(config.Certificates) == 0 && config.GetCertificate == nil && config.GetConfigForClient == nil {
+		return nil, errors.New("need valid tls.Config")
+	}
+	l, err := requestListener(network, address, true)
+	if err != nil {
+		return nil, err
+	}
+	return tls.NewListener(l, config), nil
+}
+
+func requestListener(network, address string, isTLS bool) (net.Listener, error) {
+	if uc == nil {
+		return net.Listen(network, address)
+	}
+	buf := make(memio.Buffer, 0, len(network)+len(address)+5)
+	w := byteio.StickyLittleEndianWriter{Writer: &buf}
+	w.WriteString16(network)
+	w.WriteString16(address)
+	w.WriteBool(isTLS)
+	var (
+		errNum [4]byte
+		oob    [8]byte
+	)
+	ucMu.Lock()
+	uc.Write(buf)
+	_, oobn, flags, _, err := uc.ReadMsgUnix(errNum[:], oob[:])
+	ucMu.Unlock()
+
+	return nil, nil
 }
