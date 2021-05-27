6e6ad3f787ca893db1e1c196e3b43f57450a27fd
Michael Woolnough <michael.woolnough@gmail.com>
2021-05-27 20:02:09 +0100

    start of proxt type

--- proxy.go
+++ proxy.go
@@ -1,5 +1,16 @@
 package reverseproxy // import "vimagination.zapto.org/reverseproxy"
 
+import "net"
+
+type Listener struct {
+	net.Listener
+	sockets []socket
+}
+
+type Proxy struct {
+	Listeners map[net.Addr]*Listener
+}
+
 type socket struct{}
 
 type conn struct{}
