e3e43e15e0446a03596fc41c2f4c49697f050fd9
Michael Woolnough <michael.woolnough@gmail.com>
2021-05-28 11:51:12 +0100

    added addService method for registering port services

--- proxy.go
+++ proxy.go
@@ -1,16 +1,46 @@
 package reverseproxy // import "vimagination.zapto.org/reverseproxy"
 
-import "net"
+import (
+	"fmt"
+	"net"
+	"sync"
+)
 
 type Listener struct {
 	net.Listener
-	sockets []socket
+
+	mu      sync.RWMutex
+	sockets []*socket
 }
 
 type Proxy struct {
-	Listeners map[net.Addr]*Listener
+	Listeners map[uint16]*Listener
 }
 
-type socket struct{}
+func (p *Proxy) addService(s service, port uint16) (*socket, error) {
+	l, ok := p.Listeners[port]
+	if !ok {
+		nl, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+		if err != nil {
+			return nil, err
+		}
+		l = &Listener{
+			Listener: nl,
+		}
+	}
+	socket := &socket{
+		listener: l,
+		service:  service,
+	}
+	l.mu.Lock()
+	l.sockets = append(l.sockets, socket)
+	l.mu.Unlock()
+	return socket, nil
+}
+
+type socket struct {
+	listener *listener
+	service  service
+}
 
 type conn struct{}
