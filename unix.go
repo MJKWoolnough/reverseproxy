e4be2a159da96aac3d982c717ed2dcefbfbf7c52
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-01 13:13:19 +0100

    implemented simpler unix fn

--- unix.go
+++ unix.go
@@ -9,62 +9,66 @@ import (
 
 const maxBufSize = 1<<16 + 1<<16 + 2 + 2 + 1
 
-type unixPacket struct {
-	*socket
-	*conn
+type unixService struct {
+	matchServiceName
+	conn *net.UnixConn
 }
 
-func RegisterCmd(service service, cmd *exec.Cmd) error {
+func (u *unixService) Transfer(buf []byte, conn net.Conn) {
+	cf, ok := conn.(interface{ File() (*os.File, error) })
+	if !ok {
+		conn.Close()
+		return
+	}
+	f, err := cf.File()
+	if err != nil {
+		conn.Close()
+		return
+	}
+	u.conn.WriteMsgUnix(buf, syscall.UnixRights(int(f.Fd())), nil)
+}
+
+func RegisterCmd(msn matchServiceName, cmd *exec.Cmd) error {
 	fds, err := syscall.Socketpair(syscall.AF_UNIX, syscall.SOCK_STREAM, 0)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	fconn, _ := net.FileConn(os.NewFile(uintptr(fds[0]), ""))
 	conn := fconn.(*net.UnixConn)
 	cmd.ExtraFiles = append([]*os.File{}, os.NewFile(uintptr(fds[1]), ""))
 	if err := cmd.Start(); err != nil {
-		return nil, err
+		return err
 	}
-	us := make(unixServer)
-	ns := make(chan uint16)
-	socket2ID := make(map[*socket]uint16)
-	id2Socket := make(map[uint16]*socket)
 	go func() {
-		var buf [2]byte
+		var (
+			buf  [2]byte
+			open = make(map[uint16]*Port)
+			srv  = &unixService{
+				matchServiceName: msn,
+				conn:             conn,
+			}
+		)
 		for {
 			n, _, _, _, err := conn.ReadMsgUnix(buf[:], nil)
 			if n < 2 {
 				continue
 			}
-			port := uint16(byte[1]<<8) | uint16(byte[0])
-			s, ok := id2Socket[port]
-			if !ok {
-				ns <- port
+			port := uint16(buf[1])<<8 | uint16(buf[0])
+			if p, ok := open[port]; ok {
+				p.Close()
 			} else {
-				s.Close()
-				// close socket
-			}
-			// read unix conn, get listen details
-			// read unix conn, get close details
-			// register listener
-			// listen on listener -> forward close/data
-		}
-	}()
-	go func() {
-		for {
-			select {
-			case c, ok := <-us:
-				if ok {
-					if id, ok := socket2ID[c.socket]; ok {
-
-					} else {
-
-					}
-				} else {
-
+				p, err = addPort(port, srv)
+				if err != nil {
+					errStr := err.Error()
+					b := make([]byte, 2, 2+len(errStr))
+					b[0] = buf[0]
+					b[1] = buf[1]
+					b = append(b, errStr...)
+					conn.WriteMsgUnix(b, nil, nil)
+					continue
 				}
-			case s := <-ns:
-
+				open[port] = p
+				conn.WriteMsgUnix(buf[:], nil, nil)
 			}
 		}
 	}()
