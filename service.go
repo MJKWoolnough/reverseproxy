3ef8004bd89d56afb7ac9a1cc9971f91cca452f3
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-03 09:42:16 +0100

    added type comments

--- service.go
+++ service.go
@@ -6,18 +6,21 @@ type matchServiceName interface {
 	MatchService(string) bool
 }
 
+// HostName represents an exact hostname to match on
 type HostName string
 
 func (h HostName) MatchService(serviceName string) bool {
 	return string(h) == serviceName
 }
 
+// HostNameSuffix represents a partial hostname to match the end on
 type HostNameSuffix string
 
 func (h HostNameSuffix) MatchService(serviceName string) bool {
 	return strings.HasSuffix(serviceName, string(h))
 }
 
+// Hosts represents a list of servicenames to match against
 type Hosts []matchServiceName
 
 func (h Hosts) MatchService(serviceName string) bool {
