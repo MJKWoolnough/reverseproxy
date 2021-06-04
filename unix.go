7fd748d8b1a2ccd25e38c7dc34b7c3fcd9cb99d2
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-04 10:24:06 +0100

    split run loop into seperate func

--- unix.go
+++ unix.go
@@ -38,46 +38,48 @@ func RegisterCmd(msn MatchServiceName, cmd *exec.Cmd) error {
 	if err != nil {
 		return err
 	}
-	go func() {
-		var (
-			buf  [2]byte
-			open = make(map[uint16]*Port)
-			srv  = &unixService{
-				MatchServiceName: msn,
-				conn:             conn,
+	go runCmdLoop(msn, conn)
+	return nil
+}
+
+func runCmdLoop(msn MatchServiceName, conn *net.UnixConn) {
+	var (
+		buf  [2]byte
+		open = make(map[uint16]*Port)
+		srv  = &unixService{
+			MatchServiceName: msn,
+			conn:             conn,
+		}
+	)
+	for {
+		n, _, _, _, err := conn.ReadMsgUnix(buf[:], nil)
+		if err != nil {
+			for _, p := range open {
+				p.Close()
 			}
-		)
-		for {
-			n, _, _, _, err := conn.ReadMsgUnix(buf[:], nil)
+			conn.Close()
+			return
+		}
+		if n < 2 {
+			continue
+		}
+		port := uint16(buf[1])<<8 | uint16(buf[0])
+		if p, ok := open[port]; ok {
+			delete(open, port)
+			p.Close()
+		} else {
+			p, err = addPort(port, srv)
 			if err != nil {
-				for _, p := range open {
-					p.Close()
-				}
-				conn.Close()
-				return
-			}
-			if n < 2 {
+				errStr := err.Error()
+				b := make([]byte, 2, 2+len(errStr))
+				b[0] = buf[0]
+				b[1] = buf[1]
+				b = append(b, errStr...)
+				conn.WriteMsgUnix(b, nil, nil)
 				continue
 			}
-			port := uint16(buf[1])<<8 | uint16(buf[0])
-			if p, ok := open[port]; ok {
-				delete(open, port)
-				p.Close()
-			} else {
-				p, err = addPort(port, srv)
-				if err != nil {
-					errStr := err.Error()
-					b := make([]byte, 2, 2+len(errStr))
-					b[0] = buf[0]
-					b[1] = buf[1]
-					b = append(b, errStr...)
-					conn.WriteMsgUnix(b, nil, nil)
-					continue
-				}
-				open[port] = p
-				conn.WriteMsgUnix(buf[:], nil, nil)
-			}
+			open[port] = p
+			conn.WriteMsgUnix(buf[:], nil, nil)
 		}
-	}()
-	return nil
+	}
 }
