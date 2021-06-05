444f8532286387b5662a6b6416511ae8ea8831af
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-05 11:38:31 +0100

    added initial test (an error test)

--- unixconn/unixconn_test.go
+++ unixconn/unixconn_test.go
@@ -17,11 +17,28 @@ func TestUnixConn(t *testing.T) {
 	go testServerLoop(fconn.(*net.UnixConn))
 	fconn, _ = net.FileConn(os.NewFile(uintptr(fds[1]), ""))
 	uc = fconn.(*net.UnixConn)
+	defer uc.Close()
 	fallback = false
 	newSocket = make(chan error)
 	sockets = make(map[uint16]chan net.Conn)
 	go runListenLoop()
+	l, err := Listen("tcp", ":8080")
+	if l != nil {
+		t.Error("expecting nil listener")
+		return
+	}
+	if err == nil {
+		t.Errorf("expecting 'error', got: nil")
+		return
+	} else if err.Error() != "error" {
+		t.Errorf("expecting 'error', got: %x", err)
+		return
+	}
 }
 
 func testServerLoop(conn *net.UnixConn) {
+	buf := [...]byte{0, 0, 'e', 'r', 'r', 'o', 'r'}
+	conn.ReadMsgUnix(buf[:2], nil)
+	conn.WriteMsgUnix(buf[:], nil, nil)
+	conn.Close()
 }
