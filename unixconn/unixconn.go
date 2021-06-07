b9b8beb688cb1d35182997e2de2bfe4a6251efad
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-07 07:36:41 +0100

    need to set flag (zero the port) to signify that a listener is closed so we don't resend close signal (which would actually reopen)

--- unixconn/unixconn.go
+++ unixconn/unixconn.go
@@ -150,6 +150,10 @@ func (l *listener) Accept() (net.Conn, error) {
 }
 
 func (l *listener) Close() error {
+	if l.socket == 0 {
+		return net.ErrClosed
+	}
+	l.socket = 0
 	var buf [2]byte
 	buf[0] = byte(l.socket)
 	buf[1] = byte(l.socket >> 8)
