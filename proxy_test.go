c6edca6c128b6f39a8cc78ac728b18bd1e076e18
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-11 12:13:37 +0100

    go rid of mockdns stuff, will just set the data manually

--- proxy_test.go
+++ proxy_test.go
@@ -2,29 +2,13 @@ package reverseproxy
 
 import (
 	"net"
-	"os"
 	"testing"
-
-	"github.com/foxcpp/go-mockdns"
 )
 
 const (
 	aDomain = "aaa.com"
 	bDomain = "bbb.com"
 )
-
-func TestMain(m *testing.M) {
-	addrs := mockdns.Zone{A: []string{"127.0.0.1"}}
-	srv, _ := mockdns.NewServer(map[string]mockdns.Zone{
-		aDomain: addrs,
-		bDomain: addrs,
-	}, false)
-	srv.PatchNet(net.DefaultResolver)
-	code := m.Run()
-	srv.Close()
-	mockdns.UnpatchNet(net.DefaultResolver)
-	os.Exit(code)
-}
 
 type testData struct {
 	buf  []byte
