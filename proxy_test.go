cae6128167670c4c74f847322ad732168c45e64a
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-12 08:01:30 +0100

    added first test, transferring data

--- proxy_test.go
+++ proxy_test.go
@@ -1,7 +1,9 @@
 package reverseproxy
 
 import (
+	"fmt"
 	"net"
+	"os"
 	"testing"
 )
 
@@ -52,5 +54,27 @@ func getUnusedPort() uint16 {
 }
 
 func TestListener(t *testing.T) {
-
+	pa := getUnusedPort()
+	sa := make(testService)
+	p, err := addPort(pa, testServiceA{sa})
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+		return
+	}
+	const firstSend = "GET / HTTP/1.1\r\nHost: " + aDomain + "\r\n\r\n"
+	go func() {
+		c, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", pa))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		c.Write([]byte(firstSend))
+		c.Close()
+	}()
+	data := <-sa
+	if string(data.buf) != firstSend {
+		t.Errorf("test 1: expecting buf to equal %q, got %q", firstSend, data.buf)
+		return
+	}
+	p.Close()
 }
