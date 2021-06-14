5bb7403275f687942499a00a3e1657bbdb92db02
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-14 08:48:19 +0100

    added basic structure for creating psuedo TLS handshake

---
+++ tls_test.go
@@ -0,0 +1,27 @@
+package reverseproxy
+
+var tlsBase = [...]byte{
+	22,   // TLS Handshake
+	3, 3, // Version
+	0, 0, // Length of TLS Fragment
+	1,       // CLIENT_HELLO
+	0, 0, 0, // Length of body
+	3, 3, // Version
+	0, 0, 0, 0, // Unix Time
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, // Random
+	0,    // Session ID
+	0, 2, // Cipher Suite Length
+	0, 0, // Cipher Suite
+	1, 0, // Compression (Length + ) Method
+}
+
+func tlsServerName(name string) []byte {
+	l := len(name)
+	buf := make([]byte, len(tlsBase), len(tlsBase)+11+l)
+	copy(buf, tlsBase[:])
+	buf[3] = byte((56 + l) >> 8)
+	buf[4] = byte(56 + l)
+	buf[7] = byte((52 + l) >> 8)
+	buf[8] = byte(52 + l)
+	return append(append(buf, byte((l+9)>>8), byte(l+9), 0, 0, byte((l+5)>>8), byte(l+5), byte((l+3)>>8), byte(l+3), 0, byte(l>>8), byte(l)), name...)
+}
