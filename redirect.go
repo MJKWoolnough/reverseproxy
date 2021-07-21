5adbdb677f5aee01d55c263c25d53c093ab5d5b5
Michael Woolnough <michael.woolnough@gmail.com>
2021-07-21 14:02:23 +0100

    keep track of how many conns are active

--- redirect.go
+++ redirect.go
@@ -3,28 +3,36 @@ package reverseproxy
 import (
 	"io"
 	"net"
+	"sync/atomic"
 )
 
 type addrService struct {
 	MatchServiceName
 	net.Addr
+	copying uint64
 }
 
 func (a *addrService) Transfer(buf []byte, conn *net.TCPConn) error {
 	p, err := net.Dial(a.Network(), a.String())
 	if err == nil {
 		if _, err = p.Write(buf); err == nil {
-			go copyConn(p, conn)
-			go copyConn(conn, p)
+			atomic.AddUint64(&a.copying, 2)
+			go copyConn(p, conn, &a.copying)
+			go copyConn(conn, p, &a.copying)
 		}
 	}
 	return err
 }
 
-func copyConn(a, b net.Conn) {
+func (a *addrService) Active() bool {
+	return atomic.LoadUint64(*a.copying) > 0
+}
+
+func copyConn(a, b net.Conn, c *uint64) {
 	io.Copy(a, b)
 	a.Close()
 	b.Close()
+	atomic.AddUint64(c, -1)
 }
 
 // AddRedirect sets a port to be redirected to an external service
