6c0a4c51ac1fac7044059835063d9f2bab480ed8
Michael Woolnough <michael.woolnough@gmail.com>
2021-08-03 20:49:23 +0100

    added IDs to sockets + store map of sockets (for later broadcasts)

--- cmd/reverseproxy/rpc.go
+++ cmd/reverseproxy/rpc.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"sync"
 
 	"golang.org/x/net/websocket"
 	"vimagination.zapto.org/jsonrpc"
@@ -9,12 +10,30 @@ import (
 
 type socket struct {
 	*jsonrpc.Server
+	id uint64
 }
+
+var (
+	connMu sync.RWMutex
+	conns  = make(map[*socket]struct{})
+	nextID uint64
+)
 
 func NewConn(conn *websocket.Conn) {
 	var s socket
 	s.Server = jsonrpc.New(conn, &s)
+
+	connMu.Lock()
+	nextID++
+	s.id = nextID
+	conns[&s] = struct{}{}
+	connMu.Unlock()
+
 	s.Handle()
+
+	connMu.Lock()
+	delete(conns, &s)
+	connMu.Unlock()
 }
 
 func (s *socket) HandleRPC(method string, data json.RawMessage) (interface{}, error) {
