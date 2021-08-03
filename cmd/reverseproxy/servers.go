5e99e7148c4d835c78acc1125c1872d65612bdca
Michael Woolnough <michael.woolnough@gmail.com>
2021-08-03 21:22:49 +0100

    added JSON tags

--- cmd/reverseproxy/servers.go
+++ cmd/reverseproxy/servers.go
@@ -3,16 +3,17 @@ package main
 type servers map[string]server
 
 type server struct {
-	Redirects []redirect
-	Commands  []command
+	Redirects []redirect `json:"redirects"`
+	Commands  []command  `json:"commands"`
 }
 
 type redirect struct {
-	From, To uint16
+	From uint16 `json:"from"`
+	To   uint16 `json:"to"`
 }
 
 type command struct {
-	Exe    string
-	Params []string
-	Env    map[string]string
+	Exe    string            `json:"exe"`
+	Params []string          `json:"params"`
+	Env    map[string]string `json:"env"`
 }
