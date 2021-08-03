2b3f251e831b9995041c506e9e977da16f826880
Michael Woolnough <michael.woolnough@gmail.com>
2021-08-03 20:01:36 +0100

    added flag to setup/modify config file

--- cmd/reverseproxy/main.go
+++ cmd/reverseproxy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"crypto/sha256"
 	"encoding/json"
 	"errors"
@@ -10,6 +11,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 
 	"golang.org/x/net/websocket"
 )
@@ -102,12 +104,18 @@ func main() {
 }
 
 func run() error {
-	var configFile, logFile string
+	var (
+		configFile string
+		define     bool
+	)
 	flag.StringVar(&configFile, "c", "", "config file")
-	flag.StringVar(&logFile, "l", "-", "log file")
+	flag.BoolVar(&define, "d", false, "define settings for config file")
 	flag.Parse()
 	if configFile == "" {
 		return errors.New("no config file specified")
+	}
+	if define {
+		return defineConfig(configFile)
 	}
 	var c config
 	f, err := os.Open(configFile)
@@ -135,4 +143,102 @@ func run() error {
 		return fmt.Errorf("error closing management interface: %w", err)
 	}
 	return nil
+}
+
+func defineConfig(configFile string) error {
+	var c config
+	f, err := os.Open(configFile)
+	if err == nil {
+		json.NewDecoder(f).Decode(&c)
+		f.Close()
+	}
+	r := bufio.NewReader(os.Stdin)
+	var skipPort, skipCredentials bool
+	if c.Port != 0 {
+		if err := getInput(r, fmt.Sprintf("Do you want to set a new management port (%d)? Y/N: ", c.Port), func(ans string) bool {
+			switch ans {
+			case "Y", "y":
+				skipPort = true
+			case "N", "n":
+			default:
+				return false
+			}
+			return true
+		}); err != nil {
+			return err
+		}
+	}
+	if !skipPort {
+		if err := getInput(r, "Please enter a port number for the management console (1-65535): ", func(ans string) bool {
+			p, err := strconv.ParseUint(ans, 10, 16)
+			if err != nil {
+				return false
+			}
+			c.Port = uint16(p)
+			return true
+		}); err != nil {
+			return err
+		}
+	}
+	if c.Username != "" {
+		if err := getInput(r, "Do you want to set new management credentials? Y/N: ", func(ans string) bool {
+			switch ans {
+			case "Y", "y":
+				skipCredentials = true
+			case "N", "n":
+			default:
+				return false
+			}
+			return true
+		}); err != nil {
+			return err
+		}
+	}
+	if !skipCredentials {
+		if err := getInput(r, "Username: ", func(ans string) bool {
+			if ans == "" {
+				return false
+			}
+			c.Username = ans
+			return true
+		}); err != nil {
+			return err
+		}
+		if err := getInput(r, "Password: ", func(ans string) bool {
+			if ans == "" {
+				return false
+			}
+			c.Password = sha256.Sum256([]byte(ans))
+			return true
+		}); err != nil {
+			return err
+		}
+	}
+	if !skipPort && !skipCredentials {
+		f, err := os.Create(configFile)
+		if err != nil {
+			return fmt.Errorf("error creating new config file: %w", err)
+		}
+		if err = json.NewEncoder(f).Encode(&c); err != nil {
+			return fmt.Errorf("error writing config file: %w", err)
+		}
+		if err = f.Close(); err != nil {
+			return fmt.Errorf("error closing config file: %w", err)
+		}
+	}
+	return nil
+}
+
+func getInput(r *bufio.Reader, question string, checkFn func(string) bool) error {
+	for {
+		fmt.Print(question)
+		ans, err := r.ReadString('\n')
+		if err != nil {
+			return fmt.Errorf("error returned when reading stdin: %w", err)
+		}
+		if checkFn(ans[:len(ans)-1]) {
+			return nil
+		}
+		fmt.Println("\nDid not understand response")
+	}
 }
