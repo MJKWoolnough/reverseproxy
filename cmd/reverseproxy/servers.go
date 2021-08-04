88016861d09ec049b9e17795de3a542ffdd507b5
Michael Woolnough <michael.woolnough@gmail.com>
2021-08-04 09:01:03 +0100

    added basic Init methods

--- cmd/reverseproxy/servers.go
+++ cmd/reverseproxy/servers.go
@@ -1,12 +1,33 @@
 package main
 
-type servers map[string]server
+type servers map[string]*server
+
+func (s server) Init() {
+	for _, server := range s {
+		server.Init()
+	}
+}
 
 type server struct {
 	Redirects map[uint64]redirect `json:"redirects"`
 	Commands  map[uint64]command  `json:"commands"`
-	nextRID   uint64              `json:"-"`
-	nextCID   uint64              `json:"-"`
+	lastRID   uint64              `json:"-"`
+	lastCID   uint64              `json:"-"`
+}
+
+func (s *server) Init() {
+	for id, r := range s.Redirects {
+		r.Init()
+		if id > s.lastRID {
+			s.lastRID = id
+		}
+	}
+	for id, c := range s.Commands {
+		c.Init()
+		if id > s.lastCID {
+			s.lastCID = id
+		}
+	}
 }
 
 type redirect struct {
@@ -14,8 +35,12 @@ type redirect struct {
 	To   uint16 `json:"to"`
 }
 
+func (r *redirect) Init() {}
+
 type command struct {
 	Exe    string            `json:"exe"`
 	Params []string          `json:"params"`
 	Env    map[string]string `json:"env"`
 }
+
+func (c *command) Init() {}
