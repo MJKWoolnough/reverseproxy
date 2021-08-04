1f527560e1f248203726c306bbd53d2d36571561
Michael Woolnough <michael.woolnough@gmail.com>
2021-08-04 10:16:42 +0100

    don't need to pass along name, need to store match params for each command/redirect

--- cmd/reverseproxy/servers.go
+++ cmd/reverseproxy/servers.go
@@ -1,12 +1,16 @@
 package main
 
-import "sync"
+import (
+	"sync"
+
+	"vimagination.zapto.org/reverseproxy"
+)
 
 type servers map[string]*server
 
 func (s servers) Init() {
-	for name, server := range s {
-		server.Init(name)
+	for _, server := range s {
+		server.Init()
 	}
 }
 
@@ -16,19 +20,17 @@ type server struct {
 	Commands  map[uint64]*command  `json:"commands"`
 	lastRID   uint64
 	lastCID   uint64
-	name      string
 }
 
-func (s *server) Init(name string) {
-	s.name = name
+func (s *server) Init() {
 	for id, r := range s.Redirects {
-		r.Init(name)
+		r.Init()
 		if id > s.lastRID {
 			s.lastRID = id
 		}
 	}
 	for id, c := range s.Commands {
-		c.Init(name)
+		c.Init()
 		if id > s.lastCID {
 			s.lastCID = id
 		}
@@ -61,16 +63,53 @@ func (s *server) addCommand(exe string, params []string, env map[string]string) 
 }
 
 type redirect struct {
-	From uint16 `json:"from"`
-	To   uint16 `json:"to"`
+	From             uint16  `json:"from"`
+	To               uint16  `json:"to"`
+	Match            []match `json:"match"`
+	matchServiceName reverseproxy.MatchServiceName
 }
 
-func (r *redirect) Init(name string) {}
+func (r *redirect) Init() {
+	r.matchServiceName = makeMatchService(r.Match)
+}
 
 type command struct {
-	Exe    string            `json:"exe"`
-	Params []string          `json:"params"`
-	Env    map[string]string `json:"env"`
+	Exe              string            `json:"exe"`
+	Params           []string          `json:"params"`
+	Env              map[string]string `json:"env"`
+	Match            []match           `json:"match"`
+	matchServiceName reverseproxy.MatchServiceName
 }
 
-func (c *command) Init(name string) {}
+func (c *command) Init() {
+	c.matchServiceName = makeMatchService(c.Match)
+}
+
+type match struct {
+	IsSuffix bool
+	Name     string
+}
+
+func (m match) makeMatchService() reverseproxy.MatchServiceName {
+	if m.IsSuffix {
+		return reverseproxy.HostNameSuffix(m.Name)
+	}
+	return reverseproxy.HostName(m.Name)
+}
+
+func makeMatchService(match []match) reverseproxy.MatchServiceName {
+	if len(match) == 0 {
+		return none{}
+	} else if len(match) == 1 {
+		return match[0].makeMatchService()
+	}
+	ms := make(reverseproxy.Hosts, len(match))
+	for n, m := range match {
+		ms[n] = m.makeMatchService()
+	}
+	return ms
+}
+
+type none struct{}
+
+func (none) MatchService(_ string) bool { return false }
