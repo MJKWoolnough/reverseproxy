006cfa477c0940a924cbb8d549964f94daff57a7
Michael Woolnough <michael.woolnough@gmail.com>
2021-08-08 12:22:29 +0100

    added getRedirect/getCommand methods to simplify endpoints

--- cmd/reverseproxy/rpc.go
+++ cmd/reverseproxy/rpc.go
@@ -183,6 +183,61 @@ func broadcast(id int, data json.RawMessage, except uint64) {
 	connMu.RUnlock()
 }
 
+type nameID struct {
+	Server string `json:"server"`
+	ID     uint64 `json:"id"`
+}
+
+func (s *socket) getRedirect(n nameID, fn func(*server, *redirect) error) error {
+	config.mu.Lock()
+	serv, ok := config.Servers[n.Server]
+	if !ok {
+		config.mu.Unlock()
+		return ErrNoServer
+	}
+	r, ok := serv.Redirects[n.ID]
+	if !ok {
+		config.mu.Unlock()
+		return ErrUnknownRedirect
+	}
+	if r.Start {
+		config.mu.Unlock()
+		return ErrServerRunning
+	}
+	if err := fn(serv, r); err != nil {
+		config.mu.Unlock()
+		return err
+	}
+	saveConfig()
+	config.mu.Unlock()
+	return nil
+}
+
+func (s *socket) getCommand(n nameID, fn func(*server, *command) error) error {
+	config.mu.Lock()
+	serv, ok := config.Servers[n.Server]
+	if !ok {
+		config.mu.Unlock()
+		return ErrNoServer
+	}
+	c, ok := serv.Commands[n.ID]
+	if !ok {
+		config.mu.Unlock()
+		return ErrUnknownCommand
+	}
+	if c.status == 1 {
+		config.mu.Unlock()
+		return ErrServerRunning
+	}
+	if err := fn(serv, c); err != nil {
+		config.mu.Unlock()
+		return err
+	}
+	saveConfig()
+	config.mu.Unlock()
+	return nil
+}
+
 func (s *socket) add(data json.RawMessage) (interface{}, error) {
 	var name string
 	if err := json.Unmarshal(data, &name); err != nil {
@@ -296,184 +351,80 @@ func (s *socket) addCommand(data json.RawMessage) (interface{}, error) {
 
 func (s *socket) modifyRedirect(data json.RawMessage) (interface{}, error) {
 	var mr struct {
-		Server string `json:"server"`
-		ID     uint64 `json:"id"`
+		nameID
 		redirectData
 	}
 	if err := json.Unmarshal(data, &mr); err != nil {
 		return nil, err
 	}
-	config.mu.Lock()
-	serv, ok := config.Servers[mr.Server]
-	if !ok {
-		config.mu.Unlock()
-		return nil, ErrNoServer
-	}
-	r, ok := serv.Redirects[mr.ID]
-	if !ok {
-		config.mu.Unlock()
-		return nil, ErrUnknownRedirect
-	}
-	if r.Start {
-		config.mu.Unlock()
-		return nil, ErrServerRunning
-	}
-	r.redirectData = mr.redirectData
-	saveConfig()
-	broadcast(broadcastModifyRedirect, data, s.id)
-	config.mu.Unlock()
-	return nil, nil
+	return nil, s.getRedirect(mr.nameID, func(_ *server, r *redirect) error {
+		r.redirectData = mr.redirectData
+		broadcast(broadcastModifyRedirect, data, s.id)
+		return nil
+	})
 }
 
 func (s *socket) modifyCommand(data json.RawMessage) (interface{}, error) {
 	var mc struct {
-		Server string `json:"server"`
-		ID     uint64 `json:"id"`
+		nameID
 		commandData
 	}
 	if err := json.Unmarshal(data, &mc); err != nil {
 		return nil, err
 	}
-	config.mu.Lock()
-	serv, ok := config.Servers[mc.Server]
-	if !ok {
-		config.mu.Unlock()
-		return nil, ErrNoServer
-	}
-	c, ok := serv.Commands[mc.ID]
-	if !ok {
-		config.mu.Unlock()
-		return nil, ErrUnknownCommand
-	}
-	if c.Start {
-		config.mu.Unlock()
-		return nil, ErrServerRunning
-	}
-	c.commandData = mc.commandData
-	saveConfig()
-	broadcast(broadcastModifyCommand, data, s.id)
-	config.mu.Unlock()
-	return nil, nil
+	return nil, s.getCommand(mc.nameID, func(_ *server, c *command) error {
+		c.commandData = mc.commandData
+		broadcast(broadcastModifyCommand, data, s.id)
+		return nil
+	})
 }
 
 func (s *socket) removeRedirect(data json.RawMessage) (interface{}, error) {
-	var rr struct {
-		Server string `json:"server"`
-		ID     uint64 `json:"id"`
-	}
+	var rr nameID
 	if err := json.Unmarshal(data, &rr); err != nil {
 		return nil, err
 	}
-	config.mu.Lock()
-	serv, ok := config.Servers[rr.Server]
-	if !ok {
-		config.mu.Unlock()
-		return nil, ErrNoServer
-	}
-	r, ok := serv.Redirects[rr.ID]
-	if !ok {
-		config.mu.Unlock()
-		return nil, ErrUnknownRedirect
-	}
-	if r.Start {
-		config.mu.Unlock()
-		return nil, ErrServerRunning
-	}
-	delete(serv.Redirects, rr.ID)
-	saveConfig()
-	broadcast(broadcastRemoveRedirect, data, s.id)
-	config.mu.Unlock()
-	return nil, nil
+	return nil, s.getRedirect(rr, func(serv *server, _ *redirect) error {
+		delete(serv.Redirects, rr.ID)
+		broadcast(broadcastModifyRedirect, data, s.id)
+		return nil
+	})
 }
 
 func (s *socket) removeCommand(data json.RawMessage) (interface{}, error) {
-	var rc struct {
-		Server string `json:"server"`
-		ID     uint64 `json:"id"`
-	}
+	var rc nameID
 	if err := json.Unmarshal(data, &rc); err != nil {
 		return nil, err
 	}
-	config.mu.Lock()
-	serv, ok := config.Servers[rc.Server]
-	if !ok {
-		config.mu.Unlock()
-		return nil, ErrNoServer
-	}
-	c, ok := serv.Commands[rc.ID]
-	if !ok {
-		config.mu.Unlock()
-		return nil, ErrUnknownCommand
-	}
-	if c.Start {
-		config.mu.Unlock()
-		return nil, ErrServerRunning
-	}
-	delete(serv.Commands, rc.ID)
-	saveConfig()
-	broadcast(broadcastRemoveCommand, data, s.id)
-	config.mu.Unlock()
-	return nil, nil
+	return nil, s.getCommand(rc, func(serv *server, _ *command) error {
+		delete(serv.Commands, rc.ID)
+		broadcast(broadcastRemoveCommand, data, s.id)
+		return nil
+	})
 }
 
 func (s *socket) startRedirect(data json.RawMessage) (interface{}, error) {
-	var sr struct {
-		Server string `json:"server"`
-		ID     uint64 `json:"id"`
-	}
+	var sr nameID
 	if err := json.Unmarshal(data, &sr); err != nil {
 		return nil, err
 	}
-	config.mu.Lock()
-	serv, ok := config.Servers[sr.Server]
-	if !ok {
-		config.mu.Unlock()
-		return nil, ErrNoServer
-	}
-	r, ok := serv.Redirects[sr.ID]
-	if !ok {
-		config.mu.Unlock()
-		return nil, ErrUnknownRedirect
-	}
-	if r.Start {
-		config.mu.Unlock()
-		return nil, ErrServerRunning
-	}
-	r.Run()
-	saveConfig()
-	broadcast(broadcastStartRedirect, data, s.id)
-	config.mu.Unlock()
-	return nil, nil
+	return nil, s.getRedirect(sr, func(_ *server, r *redirect) error {
+		r.Run()
+		broadcast(broadcastStartRedirect, data, s.id)
+		return nil
+	})
 }
 
 func (s *socket) startCommand(data json.RawMessage) (interface{}, error) {
-	var sc struct {
-		Server string `json:"server"`
-		ID     uint64 `json:"id"`
-	}
+	var sc nameID
 	if err := json.Unmarshal(data, &sc); err != nil {
 		return nil, err
 	}
-	config.mu.Lock()
-	serv, ok := config.Servers[sc.Server]
-	if !ok {
-		config.mu.Unlock()
-		return nil, ErrNoServer
-	}
-	c, ok := serv.Commands[sc.ID]
-	if !ok {
-		config.mu.Unlock()
-		return nil, ErrUnknownRedirect
-	}
-	if c.status == 1 {
-		config.mu.Unlock()
-		return nil, ErrServerRunning
-	}
-	c.Run()
-	saveConfig()
-	broadcast(broadcastStartCommand, data, s.id)
-	config.mu.Unlock()
-	return nil, nil
+	return nil, s.getCommand(sc, func(_ *server, c *command) error {
+		c.Run()
+		broadcast(broadcastStartCommand, data, s.id)
+		return nil
+	})
 }
 
 func stop(data json.RawMessage) (interface{}, error) {
