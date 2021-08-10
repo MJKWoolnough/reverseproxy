e539efd7f49325ae5a9ef871023f196e71e199af
Michael Woolnough <michael.woolnough@gmail.com>
2021-08-10 11:54:12 +0100

    added export RPC object to save passing it around

--- cmd/reverseproxy/internal/static/rpc.ts
+++ cmd/reverseproxy/internal/static/rpc.ts
@@ -3,8 +3,10 @@ import RPC from './lib/rpc_ws.js';
 
 const broadcastList = -1, broadcastAdd = -2, broadcastRename = -3, broadcastRemove = -4, broadcastAddRedirect = -5, broadcastAddCommand = -6, broadcastModifyRedirect = -7, broadcastModifyCommand = -8, broadcastRemoveRedirect = -9, broadcastRemoveCommand = -10, broadcastStartRedirect = -11, broadcastStartCommand = -12, broadcastStopRedirect = -13, broadcastStopCommand = -14, broadcastCommandStopped = -15, broadcastCommandError = -16;
 
+export let rpc: RPCType;
+
 export default (url: string): Promise<RPCType> => {
-	return RPC(url, 1.1).then(arpc => ({
+	return RPC(url, 1.1).then(arpc => (rpc = {
 		waitList:           () => arpc.await(broadcastList, true),
 		waitAdd:            () => arpc.await(broadcastAdd, true),
 		waitRename:         () => arpc.await(broadcastRename, true),
