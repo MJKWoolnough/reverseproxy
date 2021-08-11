29298ba2f4aa1c9ea9a6a5e126107493026602fc
Michael Woolnough <michael.woolnough@gmail.com>
2021-08-11 10:50:57 +0100

    freeze the RPC object

--- cmd/reverseproxy/internal/static/rpc.ts
+++ cmd/reverseproxy/internal/static/rpc.ts
@@ -3,10 +3,10 @@ import RPC from './lib/rpc_ws.js';
 
 const broadcastList = -1, broadcastAdd = -2, broadcastRename = -3, broadcastRemove = -4, broadcastAddRedirect = -5, broadcastAddCommand = -6, broadcastModifyRedirect = -7, broadcastModifyCommand = -8, broadcastRemoveRedirect = -9, broadcastRemoveCommand = -10, broadcastStartRedirect = -11, broadcastStartCommand = -12, broadcastStopRedirect = -13, broadcastStopCommand = -14, broadcastCommandStopped = -15, broadcastCommandError = -16;
 
-export let rpc: RPCType;
+export let rpc: Readonly<RPCType>;
 
 export default (url: string): Promise<RPCType> => {
-	return RPC(url, 1.1).then(arpc => (rpc = {
+	return RPC(url, 1.1).then(arpc => (rpc = Object.freeze({
 		waitList:           () => arpc.await(broadcastList, true),
 		waitAdd:            () => arpc.await(broadcastAdd, true),
 		waitRename:         () => arpc.await(broadcastRename, true),
@@ -37,5 +37,5 @@ export default (url: string): Promise<RPCType> => {
 		startCommand:   (data: NameID)                     => arpc.request("startCommand", data),
 		stopRedirect:   (data: NameID)                     => arpc.request("stopRedirect", data),
 		stopCommand:    (data: NameID)                     => arpc.request("stopCommand", data),
-	}));
+	})));
 };
