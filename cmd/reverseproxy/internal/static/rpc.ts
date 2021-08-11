690d3a1dc3f741ded62960f00f2c1f6f3dab0e19
Michael Woolnough <michael.woolnough@gmail.com>
2021-08-11 14:01:56 +0100

    removed needless typing

--- cmd/reverseproxy/internal/static/rpc.ts
+++ cmd/reverseproxy/internal/static/rpc.ts
@@ -1,4 +1,4 @@
-import type {RPC as RPCType, RedirectData, CommandData, NameID} from './types.js';
+import type {RPC as RPCType} from './types.js';
 import RPC from './lib/rpc_ws.js';
 
 const broadcastList = -1, broadcastAdd = -2, broadcastRename = -3, broadcastRemove = -4, broadcastAddRedirect = -5, broadcastAddCommand = -6, broadcastModifyRedirect = -7, broadcastModifyCommand = -8, broadcastRemoveRedirect = -9, broadcastRemoveCommand = -10, broadcastStartRedirect = -11, broadcastStartCommand = -12, broadcastStopRedirect = -13, broadcastStopCommand = -14, broadcastCommandStopped = -15, broadcastCommandError = -16;
@@ -24,19 +24,19 @@ export default (url: string): Promise<RPCType> => {
 		waitCommandStopped: () => arpc.await(broadcastCommandStopped, true),
 		waitCommandError:   () => arpc.await(broadcastCommandError, true),
 
-		add:             (name: string)                     => arpc.request("add", name),
-		rename:          (oldName: string, newName: string) => arpc.request("rename", [oldName, newName]),
-		remove:          (name: string)                     => arpc.request("remove", name),
-		addRedirect:     (data: RedirectData)               => arpc.request("addRedirect", data),
-		addCommand:      (data: CommandData)                => arpc.request("addCommand", data),
-		modifyRedirect:  (data: RedirectData | NameID)      => arpc.request("modifyRedirect", data),
-		modifyCommand:   (data: CommandData | NameID)       => arpc.request("modifyCommand", data),
-		removeRedirect:  (data: NameID)                     => arpc.request("removeRedirect", data),
-		removeCommand:   (data: NameID)                     => arpc.request("removeCommand", data),
-		startRedirect:   (data: NameID)                     => arpc.request("startRedirect", data),
-		startCommand:    (data: NameID)                     => arpc.request("startCommand", data),
-		stopRedirect:    (data: NameID)                     => arpc.request("stopRedirect", data),
-		stopCommand:     (data: NameID)                     => arpc.request("stopCommand", data),
-		getCommandPorts: (data: NameID)                     => arpc.request("getCommandPorts", data),
+		add:              name              => arpc.request("add", name),
+		rename:          (oldName, newName) => arpc.request("rename", [oldName, newName]),
+		remove:           name              => arpc.request("remove", name),
+		addRedirect:      data              => arpc.request("addRedirect", data),
+		addCommand:       data              => arpc.request("addCommand", data),
+		modifyRedirect:   data              => arpc.request("modifyRedirect", data),
+		modifyCommand:    data              => arpc.request("modifyCommand", data),
+		removeRedirect:   data              => arpc.request("removeRedirect", data),
+		removeCommand:    data              => arpc.request("removeCommand", data),
+		startRedirect:    data              => arpc.request("startRedirect", data),
+		startCommand:     data              => arpc.request("startCommand", data),
+		stopRedirect:     data              => arpc.request("stopRedirect", data),
+		stopCommand:      data              => arpc.request("stopCommand", data),
+		getCommandPorts:  data              => arpc.request("getCommandPorts", data),
 	})));
 };
