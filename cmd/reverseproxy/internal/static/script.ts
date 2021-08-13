572a7d0ade80c7e2aaa6ad4827a64b8c0222aa58
Michael Woolnough <michael.woolnough@gmail.com>
2021-08-13 14:15:06 +0100

    switch to class based structure

--- cmd/reverseproxy/internal/static/script.ts
+++ cmd/reverseproxy/internal/static/script.ts
@@ -1,4 +1,4 @@
-import type {Uint, Command, Match, Redirect, ListItem} from './types.js';
+import type {Uint, Match, MatchData, ListItem} from './types.js';
 import {createHTML, clearElement} from './lib/dom.js';
 import {button, div, li, span, ul} from './lib/html.js';
 import {stringSort, SortNode} from './lib/ordered.js';
@@ -6,80 +6,100 @@ import RPC from './rpc.js';
 
 declare const pageLoad: Promise<void>;
 
-type Server = {
-	name: string;
+
+const rcSort = (a: Redirect | Command, b: Redirect | Command) => a.id - b.id,
+      matchData2Match = (md: MatchData[]) => md.map(([isSuffix, name]) => ({isSuffix, name})),
+      add2Map = <K, T>(m: Map<K, T>, id: K, item: T) => {
+	      m.set(id, item);
+	      return item;
+      };
+
+class Redirect {
+	server: Server;
+	id: Uint;
+	_from: Uint;
+	_to: string;
+	active: boolean;
+	match: Match[];
 	node: HTMLLIElement;
-	redirects: SortNode<Redirect & {node: HTMLLIElement}>;
-	commands: SortNode<Command & {node: HTMLLIElement}>;
-	redirectMap: Map<Uint, Redirect>;
-	commandMap: Map<Uint, Command>;
-};
+	fromSpan: HTMLSpanElement;
+	toSpan: HTMLSpanElement;
+	constructor(server: Server, id: Uint, from: Uint, to: string, active: boolean, match: Match[]) {
+		this.server = server;
+		this.id = id;
+		this._from = from;
+		this._to = to;
+		this.active = active;
+		this.match = match;
+		this.fromSpan = span(from + ""),
+		this.toSpan = span(to);
+		this.node = li([
+			this.fromSpan,
+			this.toSpan
+		]);
+	}
+	set from(f: Uint) {
+		this.fromSpan.innerText = (this._from = f) + "";
+	}
+	set to(t: string) {
+		this.toSpan.innerText = this._to = t;
+	}
+}
+
+class Command {
+	server: Server;
+	id: Uint;
+	_exe: string;
+	_params: string[];
+	env: Record<string, string>;
+	match: Match[];
+	node: HTMLLIElement;
+	exeSpan: HTMLSpanElement;
+	constructor(server: Server, id: Uint, exe: string, params: string[], env: Record<string, string>, match: Match[]) {
+		this.server = server;
+		this.id = id;
+		this._exe = exe;
+		this._params = params;
+		this.env = env;
+		this.match = match;
+		this.exeSpan = span(exe + " " + params.join(" "));
+		this.node = li(this.exeSpan);
+	}
+	set exe (e: string) {
+		this.exeSpan.innerText = (this.exe = e) + " " + this._params.join(" ");
+	}
+	set params (p: string[]) {
+		this.exeSpan.innerText = this.exe + " " + (this._params = p).join(" ");
+	}
+}
+
+class Server {
+	name: string;
+	redirects: SortNode<Redirect>;
+	commands: SortNode<Command>;
+	redirectMap = new Map<Uint, Redirect>();
+	commandMap = new Map<Uint, Command>();
+	node: HTMLLIElement;
+	nameDiv: HTMLDivElement;
+	constructor([name, rs = [], cs = []]: ListItem) {
+		this.name = name;
+		this.redirects = new SortNode<Redirect & {node: HTMLLIElement}>(ul(), rcSort, rs.map(([id, from, to, active, _, ...match]) => add2Map(this.redirectMap, id, new Redirect(this, id, from, to, active, matchData2Match(match)))));
+		this.commands = new SortNode<Command & {node: HTMLLIElement}>(ul(), rcSort, cs.map(([id, exe, params, env, _a, _b, ...match]) => add2Map(this.commandMap, id, new Command(this, id, exe, params, env, matchData2Match(match)))));
+		this.nameDiv = div(name);
+		this.node = li([
+			this.nameDiv,
+			button({"onclick": () => {
+
+			}}, "+"),
+			this.redirects.node,
+			this.commands.node
+		]);
+	}
+}
 
 pageLoad.then(() => RPC(`ws${window.location.protocol.slice(4)}//${window.location.host}/socket`).then(rpc => {rpc.waitList().then(list => {
-	const l = new SortNode(ul(), (a: Server, b: Server) => stringSort(a.name, b.name)),
-	      rcSort = (a: Redirect | Command, b: Redirect | Command) => a.id - b.id,
-	      addToList = ([name, rs = [], cs = []]: ListItem) => {
-		const nameDiv = div(name),
-		      redirects = ul(),
-		      commands = ul(),
-		      redirectMap = new Map<Uint, Redirect>(),
-		      commandMap = new Map<Uint, Command>(),
-		      server: Server = {
-			get name(){return name},
-			set name(n: string){nameDiv.innerText = name = n},
-			node: li([
-				nameDiv,
-				button({"onclick": () => {
-
-				}}, "+"),
-				redirects,
-				commands
-			]),
-			redirects: new SortNode<Redirect & {node: HTMLLIElement}>(redirects, rcSort, rs.map(([id, from, to, active, _, ...match]) => {
-				const fromSpan = span(from + ""),
-				      toSpan = span(to),
-				      r = {
-					get server() {return name},
-					id,
-					get from() {return from},
-					set from(f: Uint) {fromSpan.innerText = (from = f) + ""},
-					get to() {return to},
-					set to(t: string) {toSpan.innerText = to = t},
-					get active() {return active},
-					set active(a: boolean) {active = a},
-					match: (match as Match[]).map(([isSuffix, name]) => ({isSuffix, name})),
-					node: li([
-						fromSpan,
-						toSpan
-					])
-				};
-				redirectMap.set(id, r);
-				return r;
-			})),
-			commands: new SortNode<Command & {node: HTMLLIElement}>(commands, rcSort, cs.map(([id, exe, params, env, ...match]) => {
-				const exeSpan = span(exe + " " + params.join(" ")),
-				      c = {
-					get server() {return server.name;},
-					id,
-					get exe() {return exe},
-					set exe(e: string) {exeSpan.innerText = (exe = e) + " " + params.join(" ")},
-					get params() {return params},
-					set params(p: string[]) {exeSpan.innerText = exe + " " + (params = p).join(" ")},
-					env,
-					match: (match as Match[]).map(([isSuffix, name]) => ({isSuffix, name})),
-					node: li(exeSpan)
-				};
-				commandMap.set(id, c);
-				return c;
-			})),
-			redirectMap,
-			commandMap
-		      };
-		l.push(server);
-		servers.set(name, server);
-	      },
-	      servers = new Map<string, Server>();
-	list.forEach(addToList);
+	const servers = new Map<string, Server>(),
+	      l = new SortNode(ul(), (a: Server, b: Server) => stringSort(a.name, b.name), list.map(i => add2Map(servers, i[0], new Server(i))));
 	createHTML(clearElement(document.body), [
 		button({"onclick": () => {
 
