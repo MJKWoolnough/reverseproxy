3e7c206c07b2e635dd2297b1f30616fa2ccce873
Michael Woolnough <michael.woolnough@gmail.com>
2021-09-15 14:13:44 +0100

    need to return buf, not nil, to avoid poisoning the cache pool

--- tls.go
+++ tls.go
@@ -15,7 +15,7 @@ func readTLSServerName(c io.Reader, buf []byte) (string, []byte, error) {
 	mbuf := memio.Buffer(buf[1:5])
 	n, err := io.ReadFull(c, mbuf)
 	if err != nil {
-		return "", nil, err
+		return "", buf, err
 	}
 	r := byteio.StickyBigEndianReader{
 		Reader: &mbuf,
@@ -27,23 +27,23 @@ func readTLSServerName(c io.Reader, buf []byte) (string, []byte, error) {
 	length := r.ReadUint16()
 
 	if cap(mbuf) < int(length) {
-		return "", nil, io.ErrShortBuffer
+		return "", buf, io.ErrShortBuffer
 	}
 
 	mbuf = mbuf[:length]
 	m, err := io.ReadFull(c, mbuf)
 	if err != nil {
-		return "", nil, err
+		return "", buf, err
 	}
 	n += m
 
 	if r.ReadUint8() != 1 {
-		return "", nil, errNoClientHello
+		return "", buf, errNoClientHello
 	}
 
 	l := r.ReadUint24()
 	if l != uint32(length)-4 {
-		return "", nil, fmt.Errorf("error reading body: %w", errInvalidLength)
+		return "", buf, fmt.Errorf("error reading body: %w", errInvalidLength)
 	}
 
 	mbuf = mbuf[1:] // skip major version
@@ -55,26 +55,26 @@ func readTLSServerName(c io.Reader, buf []byte) (string, []byte, error) {
 	sessionLength := r.ReadUint8()
 	if sessionLength > 32 || len(mbuf) < int(sessionLength) {
 		// invalid length
-		return "", nil, fmt.Errorf("error reading sesion id: %w", errInvalidLength)
+		return "", buf, fmt.Errorf("error reading sesion id: %w", errInvalidLength)
 	}
 	mbuf = mbuf[sessionLength:] // skip session id
 
 	cipherSuiteLength := r.ReadUint16()
 	if cipherSuiteLength == 0 || len(mbuf) < int(cipherSuiteLength) {
 		// invalid length
-		return "", nil, fmt.Errorf("error reading cipher suites: %w", errInvalidLength)
+		return "", buf, fmt.Errorf("error reading cipher suites: %w", errInvalidLength)
 	}
 	mbuf = mbuf[cipherSuiteLength:] // skip cipher suites
 
 	compressionMethodLength := r.ReadUint8()
 	if compressionMethodLength < 1 || len(mbuf) < int(compressionMethodLength) {
-		return "", nil, fmt.Errorf("error reading compressions: %w", errInvalidLength)
+		return "", buf, fmt.Errorf("error reading compressions: %w", errInvalidLength)
 	}
 	mbuf = mbuf[compressionMethodLength:] // skip compression methods
 
 	extsLength := r.ReadUint16()
 	if len(mbuf) < int(extsLength) {
-		return "", nil, fmt.Errorf("error reading extensions: %w", errInvalidLength)
+		return "", buf, fmt.Errorf("error reading extensions: %w", errInvalidLength)
 	}
 	mbuf = mbuf[:extsLength]
 
@@ -82,25 +82,25 @@ func readTLSServerName(c io.Reader, buf []byte) (string, []byte, error) {
 		extType := r.ReadUint16()
 		extLength := r.ReadUint16()
 		if len(mbuf) < int(extLength) {
-			return "", nil, fmt.Errorf("error reading extension: %w", errInvalidLength)
+			return "", buf, fmt.Errorf("error reading extension: %w", errInvalidLength)
 		}
 		if extType == 0 { // server_name
 			l := r.ReadUint16()
 			if l != extLength-2 {
-				return "", nil, fmt.Errorf("error reading server name extension: %w", errInvalidLength)
+				return "", buf, fmt.Errorf("error reading server name extension: %w", errInvalidLength)
 			}
 
 			mbuf = mbuf[1:] // skip name_type
 
 			nameLength := r.ReadUint16()
 			if len(mbuf) < int(nameLength) {
-				return "", nil, fmt.Errorf("error reading server name: %w", errInvalidLength)
+				return "", buf, fmt.Errorf("error reading server name: %w", errInvalidLength)
 			}
 			return string(mbuf[:nameLength]), buf[:n+1], nil
 		}
 		mbuf = mbuf[extLength:]
 	}
-	return "", nil, errNoName
+	return "", buf, errNoName
 }
 
 var (
