c909cc286c74702e8432446c0e313b0ce0d96872
Michael Woolnough <michael.woolnough@gmail.com>
2021-09-17 08:29:00 +0100

    replaced html/svg custom libs with soft-link

--- cmd/reverseproxy/internal/static/lib/html.ts
+++ cmd/reverseproxy/internal/static/lib/html.ts
@@ -1,6 +1,1 @@
-import type {DOMBind} from './dom.js';
-import {createHTML} from './dom.js';
-
-export {createHTML};
-
-export const [br, button, div, h1, img, input, label, li, slot, span, style, ul] = "br button div h1 img input label li slot span style ul".split(" ").map(e => createHTML.bind(null, e)) as [DOMBind<HTMLElementTagNameMap["br"]>, DOMBind<HTMLElementTagNameMap["button"]>, DOMBind<HTMLElementTagNameMap["div"]>, DOMBind<HTMLElementTagNameMap["h1"]>, DOMBind<HTMLElementTagNameMap["img"]>, DOMBind<HTMLElementTagNameMap["input"]>, DOMBind<HTMLElementTagNameMap["label"]>, DOMBind<HTMLElementTagNameMap["li"]>, DOMBind<HTMLElementTagNameMap["slot"]>, DOMBind<HTMLElementTagNameMap["span"]>, DOMBind<HTMLElementTagNameMap["style"]>, DOMBind<HTMLElementTagNameMap["ul"]>];
+/home/michael/Programming/Go/src/vimagination.zapto.org/jslib/lib.ts/html.ts
--- cmd/reverseproxy/internal/static/lib/svg.ts
+++ cmd/reverseproxy/internal/static/lib/svg.ts
@@ -1,6 +1,1 @@
-import type {DOMBind} from './dom.js';
-import {createSVG} from './dom.js';
-
-export {createSVG};
-
-export const [circle, g, line, path, polyline, rect, svg, symbol, title, use] = "circle g line path polyline rect svg symbol title use".split(" ").map(e => createSVG.bind(null, e)) as [DOMBind<SVGElementTagNameMap["circle"]>,DOMBind<SVGElementTagNameMap["g"]>,DOMBind<SVGElementTagNameMap["line"]>,DOMBind<SVGElementTagNameMap["path"]>,DOMBind<SVGElementTagNameMap["polyline"]>,DOMBind<SVGElementTagNameMap["rect"]>,DOMBind<SVGElementTagNameMap["svg"]>,DOMBind<SVGElementTagNameMap["symbol"]>,DOMBind<SVGElementTagNameMap["title"]>,DOMBind<SVGElementTagNameMap["use"]>];
+/home/michael/Programming/Go/src/vimagination.zapto.org/jslib/lib.ts/svg.ts
