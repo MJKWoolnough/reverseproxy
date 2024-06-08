6c1def899ab3cdea6c97b2b52032d0c7e8320057
Michael Woolnough <michael.woolnough@gmail.com>
2024-06-08 21:34:13 +0100

    minor cleanup

--- cmd/reverseproxy/main.go
+++ cmd/reverseproxy/main.go
@@ -23,18 +23,21 @@ func (h *hash) MarshalJSON() ([]byte, error) {
 	r := make([]byte, (sha256.Size<<1)+2)
 	r[0] = '"'
 	r[(sha256.Size<<1)+1] = '"'
+
 	for n, b := range *h {
 		if t := b >> 4; t > 9 {
 			r[(n<<1)+1] = 'A' - 10 + t
 		} else {
 			r[(n<<1)+1] = '0' + t
 		}
+
 		if t := b & 15; t > 9 {
 			r[(n<<1)+2] = 'A' - 10 + t
 		} else {
 			r[(n<<1)+2] = '0' + t
 		}
 	}
+
 	return r, nil
 }
 
@@ -44,8 +47,10 @@ func (h *hash) UnmarshalJSON(data []byte) error {
 	if len(data) != sha256.Size<<1+2 || data[0] != '"' || data[sha256.Size<<1+1] != '"' {
 		return ErrInvalidPasswordHash
 	}
+
 	for n, b := range data[1 : sha256.Size<<1+1] {
 		var v byte
+
 		if b >= '0' && b <= '9' {
 			v = b - '0'
 		} else if b >= 'A' && b <= 'F' {
@@ -55,12 +60,14 @@ func (h *hash) UnmarshalJSON(data []byte) error {
 		} else {
 			return ErrInvalidPasswordHash
 		}
+
 		if n&1 == 0 {
 			(*h)[n>>1] = v << 4
 		} else {
 			(*h)[n>>1] |= v
 		}
 	}
+
 	return nil
 }
 
@@ -83,12 +90,15 @@ func saveConfig() error {
 	if err != nil {
 		return fmt.Errorf("error creating new config file: %w", err)
 	}
+
 	if err = json.NewEncoder(f).Encode(&config); err != nil {
 		return fmt.Errorf("error writing config file: %w", err)
 	}
+
 	if err = f.Close(); err != nil {
 		return fmt.Errorf("error closing config file: %w", err)
 	}
+
 	return nil
 }
 
@@ -112,8 +122,10 @@ func (c *Config) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		default:
 			http.NotFound(w, r)
 		}
+
 		return
 	}
+
 	w.Header().Set("WWW-Authenticate", "Basic realm=\"Enter Credentials\"")
 	w.WriteHeader(http.StatusUnauthorized)
 	w.Write(unauthorised)
@@ -128,47 +140,63 @@ func main() {
 
 func run() error {
 	var define bool
+
 	flag.StringVar(&configFile, "c", "", "config file")
 	flag.BoolVar(&define, "d", false, "define settings for config file")
 	flag.Parse()
+
 	if configFile == "" {
 		return errors.New("no config file specified")
 	}
+
 	if define {
 		return defineConfig()
 	}
+
 	f, err := os.Open(configFile)
 	if err != nil {
 		return fmt.Errorf("error while opening config file: %w", err)
 	}
+
 	if err := json.NewDecoder(f).Decode(&config); err != nil {
 		return fmt.Errorf("error while decoding config file: %w", err)
 	}
+
 	f.Close()
+
 	l, err := net.ListenTCP("tcp", &net.TCPAddr{Port: int(config.Port)})
 	if err != nil {
 		return fmt.Errorf("error opening management interface port: %w", err)
 	}
+
 	if config.Servers == nil {
 		config.Servers = make(servers)
 	}
+
 	config.Servers.Init()
+
 	s := http.Server{
 		Handler: &config,
 	}
+
 	go func() {
 		if err := s.Serve(l); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	}()
+
 	sc := make(chan os.Signal, 1)
+
 	signal.Notify(sc, os.Interrupt)
+
 	<-sc
+
 	signal.Stop(sc)
 	close(sc)
 	s.Close()
 	ShutdownRPC()
 	config.Servers.Shutdown()
+
 	return nil
 }
 
@@ -180,8 +208,11 @@ func defineConfig() error {
 	} else if !os.IsNotExist(err) {
 		return fmt.Errorf("error opening config file: %w", err)
 	}
+
 	r := bufio.NewReader(os.Stdin)
+
 	var skipPort, skipCredentials bool
+
 	if config.Port != 0 {
 		if err := getInput(r, fmt.Sprintf("Do you want to set a new management port (%d)? Y/N: ", config.Port), func(ans string) bool {
 			switch ans {
@@ -191,23 +222,28 @@ func defineConfig() error {
 			default:
 				return false
 			}
+
 			return true
 		}); err != nil {
 			return err
 		}
 	}
+
 	if !skipPort {
 		if err := getInput(r, "Please enter a port number for the management console (1-65535): ", func(ans string) bool {
 			p, err := strconv.ParseUint(ans, 10, 16)
 			if err != nil {
 				return false
 			}
+
 			config.Port = uint16(p)
+
 			return true
 		}); err != nil {
 			return err
 		}
 	}
+
 	if config.Username != "" {
 		if err := getInput(r, "Do you want to set new management credentials? Y/N: ", func(ans string) bool {
 			switch ans {
@@ -217,47 +253,59 @@ func defineConfig() error {
 			default:
 				return false
 			}
+
 			return true
 		}); err != nil {
 			return err
 		}
 	}
+
 	if !skipCredentials {
 		if err := getInput(r, "Username: ", func(ans string) bool {
 			if ans == "" {
 				return false
 			}
+
 			config.Username = ans
+
 			return true
 		}); err != nil {
 			return err
 		}
+
 		if err := getInput(r, "Password: ", func(ans string) bool {
 			if ans == "" {
 				return false
 			}
+
 			config.Password = sha256.Sum256([]byte(ans))
+
 			return true
 		}); err != nil {
 			return err
 		}
 	}
+
 	if !skipPort || !skipCredentials {
 		return saveConfig()
 	}
+
 	return nil
 }
 
 func getInput(r *bufio.Reader, question string, checkFn func(string) bool) error {
 	for {
 		fmt.Print(question)
+
 		ans, err := r.ReadString('\n')
 		if err != nil {
 			return fmt.Errorf("error returned when reading stdin: %w", err)
 		}
+
 		if checkFn(ans[:len(ans)-1]) {
 			return nil
 		}
+
 		fmt.Println("\nDid not understand response")
 	}
 }
--- cmd/reverseproxy/rpc.go
+++ cmd/reverseproxy/rpc.go
@@ -46,6 +46,7 @@ func NewConn(conn *websocket.Conn) {
 	s := socket{
 		conn: conn,
 	}
+
 	s.Server = jsonrpc.New(conn, &s)
 
 	connMu.Lock()
@@ -65,9 +66,11 @@ func NewConn(conn *websocket.Conn) {
 
 func ShutdownRPC() {
 	connMu.Lock()
+
 	for c := range conns {
 		c.conn.Close()
 	}
+
 	connMu.Unlock()
 }
 
@@ -102,76 +105,100 @@ func (s *socket) HandleRPC(method string, data json.RawMessage) (interface{}, er
 	case "getCommandPorts":
 		return s.getCommandPorts(data)
 	}
+
 	return nil, nil
 }
 
 func buildInitialMessage() json.RawMessage {
 	config.mu.RLock()
+
 	buf := []byte{'['}
 	f := true
+
 	for name, server := range config.Servers {
 		if f {
 			f = false
 		} else {
 			buf = append(buf, ',')
 		}
+
 		buf = fmt.Appendf(buf, "[%q,[", name)
 		first := true
+
 		for id, redirect := range server.Redirects {
 			if first {
 				first = false
 			} else {
 				buf = append(buf, ',')
 			}
+
 			buf = fmt.Appendf(buf, "[%d,%d,%q,%t,%q,", id, redirect.From, redirect.To, redirect.Start, redirect.err)
+
 			for n, m := range redirect.Match {
 				if n > 0 {
 					buf = append(buf, ',')
 				}
+
 				buf = fmt.Appendf(buf, "[%t,%q]", m.IsSuffix, m.Name)
 			}
+
 			buf = append(buf, ']')
 		}
+
 		buf = append(buf, ']', ',', '[')
 		first = true
+
 		for id, cmd := range server.Commands {
 			if first {
 				first = false
 			} else {
 				buf = append(buf, ',')
 			}
+
 			buf = fmt.Appendf(buf, "[%d,%q,[", id, cmd.Exe)
+
 			for n, param := range cmd.Params {
 				if n > 0 {
 					buf = append(buf, ',')
 				}
+
 				buf = fmt.Appendf(buf, "%q", param)
 			}
+
 			buf = fmt.Appendf(buf, "],%q,{", cmd.WorkDir)
 			o := true
+
 			for key, value := range cmd.Env {
 				if o {
 					o = false
 				} else {
 					buf = append(buf, ',')
 				}
+
 				buf = fmt.Appendf(buf, "%q:%q", key, value)
 			}
+
 			buf = fmt.Appendf(buf, "},%d,%q,", cmd.status, cmd.err)
+
 			if cmd.User != nil {
 				buf = fmt.Appendf(buf, "{\"uid\":%d,\"gid\":%d}", cmd.User.UID, cmd.User.GID)
 			} else {
 				buf = append(buf, 'n', 'u', 'l', 'l')
 			}
+
 			for _, m := range cmd.Match {
 				buf = fmt.Appendf(buf, ",[%t,%q]", m.IsSuffix, m.Name)
 			}
+
 			buf = append(buf, ']')
 		}
+
 		buf = append(buf, ']', ']')
 	}
+
 	buf = append(buf, ']')
 	config.mu.RUnlock()
+
 	return buildMessage(-1, json.RawMessage(buf))
 }
 
@@ -180,29 +207,37 @@ const broadcastStart = "{\"id\": -0,\"result\":"
 func buildMessage(id int, data json.RawMessage) json.RawMessage {
 	l := len(broadcastStart) + len(data) + 1
 	dat := make(json.RawMessage, l)
+
 	copy(dat, broadcastStart)
 	copy(dat[len(broadcastStart):], data)
+
 	id = -id
 	if id > 9 {
 		dat[6] = '-'
 		dat[7] = byte('0' + id/10)
 	}
+
 	dat[8] = byte('0' + id%10)
 	dat[l-1] = '}'
+
 	return dat
 }
 
 func broadcast(id int, data json.RawMessage, except uint64) {
 	connMu.Lock()
+
 	var dat json.RawMessage
+
 	for c := range conns {
 		if c.id != except {
 			if len(dat) == 0 {
 				dat = buildMessage(id, data)
 			}
+
 			go c.SendData(dat)
 		}
 	}
+
 	connMu.Unlock()
 }
 
@@ -214,120 +249,157 @@ type nameID struct {
 func (s *socket) getRedirect(n nameID, fn func(*server, *redirect) error) error {
 	config.mu.Lock()
 	defer config.mu.Unlock()
+
 	serv, ok := config.Servers[n.Server]
 	if !ok {
 		return ErrNoServer
 	}
+
 	r, ok := serv.Redirects[n.ID]
 	if !ok {
 		return ErrUnknownRedirect
 	}
+
 	if r.Start {
 		return ErrServerRunning
 	}
+
 	if err := fn(serv, r); err != nil {
 		return err
 	}
+
 	if err := saveConfig(); err != nil {
 		return fmt.Errorf("error saving config: %w", err)
 	}
+
 	return nil
 }
 
 func (s *socket) getCommand(n nameID, fn func(*server, *command) error) error {
 	config.mu.Lock()
 	defer config.mu.Unlock()
+
 	serv, ok := config.Servers[n.Server]
 	if !ok {
 		return ErrNoServer
 	}
+
 	c, ok := serv.Commands[n.ID]
 	if !ok {
 		return ErrUnknownCommand
 	}
+
 	if c.status == 1 {
 		return ErrServerRunning
 	}
+
 	if err := fn(serv, c); err != nil {
 		return err
 	}
+
 	if err := saveConfig(); err != nil {
 		return fmt.Errorf("error saving config: %w", err)
 	}
+
 	return nil
 }
 
 func (s *socket) add(data json.RawMessage) (interface{}, error) {
 	var name string
+
 	if err := json.Unmarshal(data, &name); err != nil {
 		return nil, err
 	}
+
 	config.mu.Lock()
 	defer config.mu.Unlock()
+
 	if _, ok := config.Servers[name]; ok {
 		return nil, ErrNameExists
 	}
+
 	config.Servers[name] = &server{
 		Redirects: make(map[uint64]*redirect),
 		Commands:  make(map[uint64]*command),
 		name:      name,
 	}
+
 	if err := saveConfig(); err != nil {
 		return nil, fmt.Errorf("error saving config: %w", err)
 	}
+
 	broadcast(broadcastAdd, data, s.id)
+
 	return nil, nil
 }
 
 func (s *socket) rename(data json.RawMessage) (interface{}, error) {
 	var name [2]string
+
 	if err := json.Unmarshal(data, &name); err != nil {
 		return nil, err
 	}
+
 	config.mu.Lock()
 	defer config.mu.Unlock()
+
 	if _, ok := config.Servers[name[1]]; ok {
 		return nil, ErrNameExists
 	}
+
 	serv, ok := config.Servers[name[0]]
 	if !ok {
 		return nil, ErrNoServer
 	}
+
 	delete(config.Servers, name[0])
+
 	config.Servers[name[1]] = serv
+
 	if err := saveConfig(); err != nil {
 		return nil, fmt.Errorf("error saving config: %w", err)
 	}
+
 	broadcast(broadcastRename, data, s.id)
+
 	return nil, nil
 }
 
 func (s *socket) remove(data json.RawMessage) (interface{}, error) {
 	var name string
+
 	if err := json.Unmarshal(data, &name); err != nil {
 		return nil, err
 	}
+
 	config.mu.Lock()
 	defer config.mu.Unlock()
+
 	serv, ok := config.Servers[name]
 	if !ok {
 		return nil, ErrNoServer
 	}
+
 	for _, r := range serv.Redirects {
 		if r.Start {
 			return nil, ErrServerRunning
 		}
 	}
+
 	for _, c := range serv.Commands {
 		if c.status != 0 {
 			return nil, ErrServerRunning
 		}
 	}
+
 	delete(config.Servers, name)
+
 	if err := saveConfig(); err != nil {
 		return nil, fmt.Errorf("error saving config: %w", err)
 	}
+
 	broadcast(broadcastRemove, data, s.id)
+
 	return nil, nil
 }
 
@@ -336,20 +408,27 @@ func (s *socket) addRedirect(data json.RawMessage) (interface{}, error) {
 		Server string `json:"server"`
 		redirectData
 	}
+
 	if err := json.Unmarshal(data, &ar); err != nil {
 		return nil, err
 	}
+
 	config.mu.Lock()
 	defer config.mu.Unlock()
+
 	serv, ok := config.Servers[ar.Server]
 	if !ok {
 		return nil, ErrNoServer
 	}
+
 	id := serv.addRedirect(ar.redirectData)
+
 	if err := saveConfig(); err != nil {
 		return nil, fmt.Errorf("error saving config: %w", err)
 	}
+
 	broadcast(broadcastAddRedirect, append(strconv.AppendUint(append(data[:len(data)-1], ",\"id\":"...), id, 10), '}'), s.id)
+
 	return id, nil
 }
 
@@ -358,20 +437,27 @@ func (s *socket) addCommand(data json.RawMessage) (interface{}, error) {
 		Server string `json:"server"`
 		commandData
 	}
+
 	if err := json.Unmarshal(data, &ac); err != nil {
 		return nil, err
 	}
+
 	config.mu.Lock()
 	defer config.mu.Unlock()
+
 	serv, ok := config.Servers[ac.Server]
 	if !ok {
 		return nil, ErrNoServer
 	}
+
 	id := serv.addCommand(ac.commandData)
+
 	if err := saveConfig(); err != nil {
 		return nil, fmt.Errorf("error saving config: %w", err)
 	}
+
 	broadcast(broadcastAddCommand, append(strconv.AppendUint(append(data[:len(data)-1], ",\"id\":"...), id, 10), '}'), s.id)
+
 	return id, nil
 }
 
@@ -380,9 +466,11 @@ func (s *socket) modifyRedirect(data json.RawMessage) (interface{}, error) {
 		nameID
 		redirectData
 	}
+
 	if err := json.Unmarshal(data, &mr); err != nil {
 		return nil, err
 	}
+
 	return nil, s.getRedirect(mr.nameID, func(_ *server, r *redirect) error {
 		r.redirectData = mr.redirectData
 		broadcast(broadcastModifyRedirect, data, s.id)
@@ -395,9 +483,11 @@ func (s *socket) modifyCommand(data json.RawMessage) (interface{}, error) {
 		nameID
 		commandData
 	}
+
 	if err := json.Unmarshal(data, &mc); err != nil {
 		return nil, err
 	}
+
 	return nil, s.getCommand(mc.nameID, func(_ *server, c *command) error {
 		c.commandData = mc.commandData
 		broadcast(broadcastModifyCommand, data, s.id)
@@ -407,9 +497,11 @@ func (s *socket) modifyCommand(data json.RawMessage) (interface{}, error) {
 
 func (s *socket) removeRedirect(data json.RawMessage) (interface{}, error) {
 	var rr nameID
+
 	if err := json.Unmarshal(data, &rr); err != nil {
 		return nil, err
 	}
+
 	return nil, s.getRedirect(rr, func(serv *server, _ *redirect) error {
 		delete(serv.Redirects, rr.ID)
 		broadcast(broadcastRemoveRedirect, data, s.id)
@@ -419,9 +511,11 @@ func (s *socket) removeRedirect(data json.RawMessage) (interface{}, error) {
 
 func (s *socket) removeCommand(data json.RawMessage) (interface{}, error) {
 	var rc nameID
+
 	if err := json.Unmarshal(data, &rc); err != nil {
 		return nil, err
 	}
+
 	return nil, s.getCommand(rc, func(serv *server, _ *command) error {
 		delete(serv.Commands, rc.ID)
 		broadcast(broadcastRemoveCommand, data, s.id)
@@ -431,9 +525,11 @@ func (s *socket) removeCommand(data json.RawMessage) (interface{}, error) {
 
 func (s *socket) startRedirect(data json.RawMessage) (interface{}, error) {
 	var sr nameID
+
 	if err := json.Unmarshal(data, &sr); err != nil {
 		return nil, err
 	}
+
 	return nil, s.getRedirect(sr, func(_ *server, r *redirect) error {
 		r.Run()
 		broadcast(broadcastStartRedirect, data, s.id)
@@ -443,9 +539,11 @@ func (s *socket) startRedirect(data json.RawMessage) (interface{}, error) {
 
 func (s *socket) startCommand(data json.RawMessage) (interface{}, error) {
 	var sc nameID
+
 	if err := json.Unmarshal(data, &sc); err != nil {
 		return nil, err
 	}
+
 	return nil, s.getCommand(sc, func(_ *server, c *command) error {
 		err := c.Run()
 		if err != nil {
@@ -453,81 +551,105 @@ func (s *socket) startCommand(data json.RawMessage) (interface{}, error) {
 		} else {
 			broadcast(broadcastStartCommand, data, s.id)
 		}
+
 		return err
 	})
 }
 
 func (s *socket) stopRedirect(data json.RawMessage) (interface{}, error) {
 	var sr nameID
+
 	if err := json.Unmarshal(data, &sr); err != nil {
 		return nil, err
 	}
+
 	config.mu.Lock()
 	defer config.mu.Unlock()
+
 	serv, ok := config.Servers[sr.Server]
 	if !ok {
 		return nil, ErrNoServer
 	}
+
 	r, ok := serv.Redirects[sr.ID]
 	if !ok {
 		return nil, ErrUnknownRedirect
 	}
+
 	if !r.Start {
 		return nil, ErrServerNotRunning
 	}
+
 	r.Stop()
 	broadcast(broadcastStopRedirect, data, s.id)
+
 	if err := saveConfig(); err != nil {
 		return nil, fmt.Errorf("error saving config: %w", err)
 	}
+
 	return nil, nil
 }
 
 func (s *socket) stopCommand(data json.RawMessage) (interface{}, error) {
 	var sc nameID
+
 	if err := json.Unmarshal(data, &sc); err != nil {
 		return nil, err
 	}
+
 	config.mu.Lock()
 	defer config.mu.Unlock()
+
 	serv, ok := config.Servers[sc.Server]
 	if !ok {
 		return nil, ErrNoServer
 	}
+
 	c, ok := serv.Commands[sc.ID]
 	if !ok {
 		return nil, ErrUnknownCommand
 	}
+
 	if c.status != 1 {
 		return nil, ErrServerNotRunning
 	}
+
 	c.Stop()
 	broadcast(broadcastStopCommand, data, s.id)
+
 	if err := saveConfig(); err != nil {
 		return nil, fmt.Errorf("error saving config: %w", err)
 	}
+
 	return nil, nil
 }
 
 func (s *socket) getCommandPorts(data json.RawMessage) (interface{}, error) {
 	var cp nameID
+
 	if err := json.Unmarshal(data, &cp); err != nil {
 		return nil, err
 	}
+
 	config.mu.Lock()
 	defer config.mu.Unlock()
+
 	serv, ok := config.Servers[cp.Server]
 	if !ok {
 		return nil, ErrNoServer
 	}
+
 	c, ok := serv.Commands[cp.ID]
 	if !ok {
 		return nil, ErrUnknownCommand
 	}
+
 	if c.status != 1 {
 		return nil, ErrServerNotRunning
 	}
+
 	ports := c.unixCmd.Status().Ports
+
 	return ports, nil
 }
 
